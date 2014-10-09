e01babce6cc66138a90a26d6bcb616aeaecc8f83
sayaka sawada <sa0104ya@gmail.com>
2014-10-09 10:35:17 +0900

    コミット

--- AttoTokushima/ViewController.m
+++ AttoTokushima/ViewController.m
@@ -40,6 +40,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    ramenArray = [NSMutableArray array];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -53,27 +54,39 @@
     //}else if (buttonCount == 20){
       //  buttonCount = 0;
     //}
-    
-    
+    //配列から取り出した要素のURLを入れる
+    NSString *ramenstring;
     //apiのURL先にあるデータを入れる
     NSData *data = [NSData dataWithContentsOfURL:url];
     NSError *err = nil;
     //dataからJSONを生成
     NSDictionary *dic = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:&err];
     NSArray *dataArray = [dic objectForKey:@"data"];
-    NSDictionary *tag_dic = [dataArray objectAtIndex:buttonCount];
+    NSLog(@"%ld",dataArray.count);
+    for (int i = 0; i < (dataArray.count); i++) {
+    NSDictionary *tag_dic = [dataArray objectAtIndex:i];
     NSDictionary *images_dic = [tag_dic objectForKey:@"images"];
     NSDictionary *standard_resolution_dic = [images_dic objectForKey:@"standard_resolution"];
     NSString *imageurl = [standard_resolution_dic objectForKey:@"url"];
     NSLog(@"%@",imageurl);
+    [ramenArray addObject:imageurl];
+    NSLog(@"%ld",ramenArray.count);
     
+    //ramenArrayからランダムに１枚取り出す
+    int pos;//ramenArrayから何番目の要素が取り出されたかを保存する箱
+    pos = arc4random()%ramenArray.count;//配列から要素をランダムに取り出す
+    ramenstring = ramenArray[pos];//該当の要素番号のURLを代入
+    NSLog(@"%d",pos);
+    }
+  
     //画像のURLをデータとしてまず入れる
-    NSData *imageData = [NSData dataWithContentsOfURL:[NSURL URLWithString:imageurl]];
+    NSData *imageData = [NSData dataWithContentsOfURL:[NSURL URLWithString:ramenstring]];
     //imgdataを画像を生成しimageに格納
     UIImage *image = [[UIImage alloc] initWithData:imageData];
+    
     //imageviewの画像をimageに設定
     self.imageview.image = image;
-    NSLog(@"%ld",buttonCount);
-    buttonCount++;
+    //NSLog(@"%ld",buttonCount);
+    //buttonCount++;
 }
 @end
