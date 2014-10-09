c12b080dccc4722579ec8a7847653f528a086c2d
sayaka sawada <sa0104ya@gmail.com>
2014-10-09 15:51:26 +0900

    コミット

---
+++ AttoTokushima/narutokintoki.h
@@ -0,0 +1,15 @@
+//
+//  narutokintoki.h
+//  AttoTokushima
+//
+//  Created by ビザンコムマック０４ on 2014/10/09.
+//  Copyright (c) 2014年 ビザンコムマック０４. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface narutokintoki : UIViewController
+@property (weak, nonatomic) IBOutlet UIImageView *narutokintokiImageVIew;
+- (IBAction)buckbutton:(UIButton *)sender;
+
+@end
