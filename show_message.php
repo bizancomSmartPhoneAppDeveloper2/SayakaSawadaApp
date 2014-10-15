ccabefbfcb62329ba9f757ac3be8e5940fe51553
sa0104ya <sa0104ya@gmail.com>
2014-10-15 15:58:07 +0900

    Update show_message.php

--- show_message.php
+++ show_message.php
@@ -4,10 +4,6 @@ $s=mysql_connect($SERV,$USER,$PASS) or die("失敗しました");
 print "<BR>接続OK! <BR><BR>";
 mysql_set_charset("utf8");
 mysql_select_db($DBNM);
-
-$a1_d=$_POST["a1"];
-$a2_d=$_POST["a2"];
-mysql_query("INSERT INTO tbkeijiban (name,message) VALUES ('$a1_d','$a2_d')");
 $re=mysql_query("SELECT * FROM tbkeijiban ORDER BY number");
 while($kekka=mysql_fetch_array($re)){
 	print $kekka[0];
